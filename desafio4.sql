c1fcd584493ca685e07cce1e948c8b93ffd9e487
RicardoCaselati <ricardocaselati@gmail.com>
2022-10-01 15:08:24 -0300

    Req 04 - OK

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT
+	u.usuario AS usuario,
+    IF (MAX(h.data_reproducao) >= '2021-01-01', 'Usuário ativo', 'Usuário inativo') AS status_usuario
+    FROM SpotifyClone.usuarios AS u
+INNER JOIN 
+	SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuarios_usuario_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
